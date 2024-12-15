cbc8f8ddcb7fff1f11ca89bb284b9e6bb73c7617
darvydas <darvydas.silkus@gmail.com>
2024-12-15 22:14:42 +0200

    Fixed terminal book table format

--- views/library_view.py
+++ views/library_view.py
@@ -11,12 +11,12 @@ def display_book(book:Book, due_date = None, reader_id = ''):
 def display_all_books(books):
   """Displays details of all books in the library."""
   if books:
-    print("-"*81)
+    print("-"*91)
     print(f"| {'Title:':<30} | {'Author:':<20} | {'Year:':<5} | {'Genre':<15} | {'Stock':<5} |")
-    print("-"*81)
+    print("-"*91)
     for book in books:
       display_book(book)
-    print("-"*81)
+    print("-"*91)
   else:
     print("No books in the library yet.")
 
@@ -24,42 +24,43 @@ def display_search_results(results):
   """Displays sea
   rch results for books."""
   if results:
-    print("-"*81)
+    print("-"*91)
     print(f"| {'Title:':<30} | {'Author:':<20} | {'Year:':<5} | {'Genre':<15} | {'Stock':<5} |")
-    print("-"*81)
+    print("-"*91)
     for book in results:
       display_book(book)
+    print("-"*91)
   else:
     print("No books found matching your query.")
 
 def display_overdue_books(overdue_books):
   """Displays details of overdue books."""
   if overdue_books:
-    print("\n"+"-"*107)
+    print("\n"+"-"*117)
     print("Overdue books:")
-    print("-"*107)
+    print("-"*117)
     print(f"| {'Reader ID:':<10} | {'Due date:':<10} | {'Title:':<30} | {'Author:':<20} | {'Year':<5} | {'Genre':<15} | {'Stock':<5} |")
-    print("-"*107)
+    print("-"*117)
     for book, borrow_info_list in overdue_books.items():
       for borrow_info in borrow_info_list:
         display_book(book, borrow_info['due_date'], borrow_info['reader_id'])
-    print("-"*107)
+    print("-"*117)
   else:
     print("No overdue books.")
 
 def display_borrowed_books (borrowed_books):
   """Displays details of borrowed books."""
   if borrowed_books:
-    print("\n"+"-"*107)
+    print("\n"+"-"*117)
     print("Borrowed books:")
-    print("-"*107)
+    print("-"*117)
     print(f"| {'Reader ID:':<10} | {'Due date:':<10} | {'Title:':<30} | {'Author:':<20} | {'Year':<5} | {'Genre':<15} | {'Stock':<5} |")
-    print("-"*107)
+    print("-"*117)
     # for book in borrowed_books:
     #   display_book(book)
     for book, borrow_info_list in borrowed_books.items():
       for borrow_info in borrow_info_list:
         display_book(book, borrow_info['due_date'], borrow_info['reader_id'])
-    print("-"*107)
+    print("-"*117)
   else:
     print("No borrowed books.")
